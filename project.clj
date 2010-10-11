ad31e85bf8ee95c4ba0c6cd9b59d4f4c91e7c92c
Alan Malloy <alan@malloys.org>
2010-10-10 22:13:51 -0700

    just realized lein defaulted me to 1.0.0 - should be 0.x.0 for sem-ver compliance

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ddsolve "1.0.0-SNAPSHOT"
+(defproject ddsolve "0.3.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
